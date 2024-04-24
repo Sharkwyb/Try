34765065c05442f58da4609b77c709735bc61c60
KMSStudent1 <KMSStudent1@kms-coding-23.cupertino.k12.ca.us>
2024-04-24 09:48:23 -0700

    New background + New question layout

--- src/Trying.java
+++ src/Trying.java
@@ -10,26 +10,26 @@ public class Trying extends JPanel implements ActionListener, MouseListener{
 	private Image qu1;
 	private Image qu2;
 	private Image qu3;
-	
+
 	private int bg_width;
 	private int bg_height;
 	private int level = 1;
 	private int scorecalculator =0;
 	private int totalscore =0;
 	private int levelPosition=0;
-	
+
 	private JLabel question; 
 	private JLabel levelnumber;
 	private JLabel scoreshowing;
 	private JLabel startingprank;
-	
+
 	private JButton qb1;
 	private JButton qb2;
 	private JButton qb3;
 	private JButton st1;
 	private JButton st2;
-	
-	
+
+
 	JButton[] correctanswers = new JButton[6];
 	String [] questionlist = new String[6];
 	String [] qb1Icon = new String[6];
@@ -37,6 +37,7 @@ public class Trying extends JPanel implements ActionListener, MouseListener{
 	String [] qb3Icon = new String[6];
 
 
+	//private int backgroundNum=0;
 
 	//switch questions
 	public void actionPerformed(ActionEvent e) {
@@ -47,7 +48,7 @@ public class Trying extends JPanel implements ActionListener, MouseListener{
 		else if (e.getSource() == st1) {
 			System.exit(0);
 		}
-		
+
 		else if (e.getSource() == correctanswers[levelPosition]) {
 			levelPosition++;
 			qb1.setVisible(true);
@@ -57,16 +58,15 @@ public class Trying extends JPanel implements ActionListener, MouseListener{
 			qb2.setIcon(new ImageIcon(qb2Icon[levelPosition]));
 			qb3.setIcon(new ImageIcon(qb3Icon[levelPosition]));
 			question.setText(questionlist[levelPosition]);
-			question.setBounds(580, 20, 600, 100);
 			levelnumber.setText("Level " + (levelPosition+1));
-			
+
 			if (levelPosition == 4) {
-				question.setBounds(400, 20, 800, 100);
-			}
-			if (levelPosition == 6) {
 				
 			}
-			
+			if (levelPosition == 6) {
+
+			}
+
 			if (scorecalculator == 0) {
 				totalscore+=3;
 			}
@@ -86,17 +86,17 @@ public class Trying extends JPanel implements ActionListener, MouseListener{
 				qb1.setVisible(false);
 				scorecalculator++;
 			}
-				
+
 			else if (e.getSource() == qb2) {
 				scorecalculator++;
 				qb2.setVisible(false);
-				
+
 			}
 			else if (e.getSource() == qb3){
- 				qb3.setVisible(false);
+				qb3.setVisible(false);
 				scorecalculator++;
 			}
-				
+
 		}
 
 
@@ -119,27 +119,27 @@ public class Trying extends JPanel implements ActionListener, MouseListener{
 		qb1Icon[2] = "src/bail3.jpg";
 		qb1Icon[3] = "src/bul3.jpg";
 		qb1Icon[4] = "src/qianfangbail5.jpeg";
-		
+
 
 		qb2Icon[0] = "src/babal1.jpeg";
 		qb2Icon[1] = "src/dianhual2.jpeg";
 		qb2Icon[2] = "src/chil3.jpg";
 		qb2Icon[3] = "src/bi4.jpeg";
-        qb2Icon[4] = "src/xianfazhiren5.jpeg";
+		qb2Icon[4] = "src/xianfazhiren5.jpeg";
 
 		qb3Icon[0] = "src/xiongdil1.jpeg";
 		qb3Icon[1] = "src/ail2.jpeg";
 		qb3Icon[2] = "src/bul3.jpg";
 		qb3Icon[3] = "src/shide4.jpeg";
 		qb3Icon[4] = "src/bukesiyi.jpeg";
-        
-		
+
+		//unbelievable
 
 		questionlist[0] = "What is \"mother\" in Chinese?";
 		questionlist[1] = "What is \"phone\" in Chinese?";
 		questionlist[2] = "What is \"white\" in Chinese?";
 		questionlist[3] = "What is \"no\" in Chinese?";
-		questionlist[4] = "What is \"unbeleiveable\" in Chinese? * CHALLENGE * ";
+		questionlist[4] = "What is \"unbelievable\" in Chinese?";
 
 
 
@@ -149,15 +149,15 @@ public class Trying extends JPanel implements ActionListener, MouseListener{
 
 		//card2.setLayout(new FlowLayout());
 		question = new JLabel(questionlist[0]);
-		question.setFont(new Font("SansSerif Plain", Font.PLAIN, 25));
-		question.setBounds(580, 20, 600, 100);
+		question.setFont(new Font("SansSerif Plain", Font.PLAIN, 40));
+		question.setBounds(650, 60, 1200, 1200);
 		card1.add(question);
 
 		scoreshowing = new JLabel("Score: " + totalscore);
 		scoreshowing.setFont(new Font("SansSerif Plain", Font.PLAIN, 25));
 		scoreshowing.setBounds(1250, 20, 300, 100);
 		card1.add(scoreshowing);
-		
+
 		levelnumber = new JLabel("Level "+ level);
 		levelnumber.setFont(new Font("SansSerif Plain", Font.PLAIN, 25));
 		levelnumber.setBounds(50, 20, 600, 100);
@@ -185,26 +185,26 @@ public class Trying extends JPanel implements ActionListener, MouseListener{
 		// Setup all the correct answers
 		correctanswers[0] = qb1;
 		correctanswers[1] = qb2;
-        correctanswers[2] = qb1;
-        correctanswers[3] = qb1;
-        correctanswers[4] = qb3;
+		correctanswers[2] = qb1;
+		correctanswers[3] = qb1;
+		correctanswers[4] = qb3;
 
-        startingprank = new JLabel("Guess which one is \"start\" in Chinese");
-        startingprank.setFont(new Font("SansSerif Plain", Font.PLAIN, 25));
+		startingprank = new JLabel("Guess which one is \"start\" in Chinese");
+		startingprank.setFont(new Font("SansSerif Plain", Font.PLAIN, 25));
 		startingprank.setBounds(300, 300, 700, 100);
 		card2.add(startingprank);
-		
+
 		st1 = new JButton("a");
 		st1.setBounds(100,100,100,100);
 		st1.addActionListener(this);
 		card2.add(st1);
-		
+
 		st2 = new JButton("b");
 		st2.setBounds(500,100,100,100);
 		st2.addActionListener(this);
 		card2.add(st2);
-		
-		
+
+
 		cards.add(card2, "start");
 		cards.add(card1, "main");
 
@@ -222,12 +222,12 @@ public class Trying extends JPanel implements ActionListener, MouseListener{
 
 	public Trying() {
 
-		ImageIcon i = new ImageIcon("src/bgg2.jpg");
+		ImageIcon i = new ImageIcon("src/bg7.jpg");
 		bg = i.getImage();
 		bg_width = bg.getWidth(null);
 		bg_height = bg.getHeight(null);
 		System.out.println("Width: " + bg_width);
-		
+
 		addMouseListener(this);
 	}
 	//background
@@ -244,8 +244,12 @@ public class Trying extends JPanel implements ActionListener, MouseListener{
 	public void paintComponent(Graphics g) {
 		//Sysout.println("Here");
 		super.paintComponent(g);
+
 		Graphics2D g2d = (Graphics2D)g;
+
+		//if (backgroundNum==0)
 		g2d.drawImage(bg, 0, 0, null);
+
 
 	}
 	// paint background method
